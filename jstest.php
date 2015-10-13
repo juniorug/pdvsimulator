cae8c05595777804b139e4c424637ed19627e3ee
juniorug <juniorug@gmail.com>
2015-10-13 10:44:26 -0300

    removendo arquivos temporarios

--- jstest.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<head>
-	<script type="text/javascript" src="js/simulator.js"></script>
-	</head>
-	<body>
-		<button class="btsend" style="width: 233px; height: 30px;" onclick="testM()"></button>
-	</body>
-</html>
